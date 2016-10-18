e48be07847163720b0b4c498e2d22e55223d62be
sNiCKY <mpruszynski@gmail.com>
2016-10-18 20:19:20 +0200

    test base case of pairs_of_cubes

--- test/pairs_of_cubes_test.rb
+++ test/pairs_of_cubes_test.rb
@@ -4,10 +4,12 @@ require_relative '../pairs_of_cubes'
 class PairsOfCubesTest < MiniTest::Test
 
   def test_pairs_of_cubes
+    result_0  = []
     result_15 = [ [[1, 12], [9, 10]] ]
     result_20 = result_15 + [ [[2, 16], [9, 15]] ]
     result_25 = result_20 + [ [[2, 24], [18, 20]] ]
 
+    assert_equal result_0,  pairs_of_cubes(0)
     assert_equal result_15, pairs_of_cubes(15)
     assert_equal result_20, pairs_of_cubes(20)
     assert_equal result_25, pairs_of_cubes(25)
